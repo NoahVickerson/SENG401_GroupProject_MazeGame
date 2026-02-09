a9a1ceb4f0ad2f30e10c722c71d4ace97546b20c
Noah Vickerson <noah.vickerson@gmail.com>
2026-02-09 08:45:40 -0700

    made the player controller work with mouse clicks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,9 @@ public class PlayerController : MonoBehaviour
   [SerializeField]
   private float velDecay;
 
+  private Vector2 targetPosition;
+  private float targetDistance;
+
     public enum Directions{
       UP,
       DOWN,
@@ -63,16 +66,24 @@ public class PlayerController : MonoBehaviour
     }
 
     private void updateVelocity(Vector2 mouseClickPosition){
-
+      float curDist = Vector2.Distance(mouseClickPosition, (Vector2)player.transform.position);
+      if(curDist < 1E-3){
+        targetPosition.x = -1;
+        targetPosition.y = -1;
+        return;
+      }
+      float velocityScalar = Math.Abs((float)(-1*Math.Pow(curDist - targetDistance/2, 2) + maxVel));
+      velocityScalar = 0.2f;
+      velocity = velocityScalar*(mouseClickPosition - (Vector2)player.transform.position)/curDist;
     }
   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
       player = gameObject;
-      Debug.Log(player.name);
         
       velocity = new Vector2(0,0);
+      targetPosition = new Vector2(-1, -1);
       pressedDirections = new List<Directions>();
 
       cam.SetCameraPosition(new Vector2(0,0));
@@ -83,20 +94,41 @@ public class PlayerController : MonoBehaviour
     {
       pressedDirections.Clear();
 
-      if(Keyboard.current.dKey.isPressed){
-        pressedDirections.Add(Directions.RIGHT);
+      if(Mouse.current.leftButton.wasPressedThisFrame){
+        // read the mouse click point
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+
+        // convert that to a point on the screen
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        targetPosition.x = mousePos.x;
+        targetPosition.y = mousePos.y;
+        targetDistance = Vector2.Distance(targetPosition, (Vector2)player.transform.position);
+      }else{
+
+        if(Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed){
+          pressedDirections.Add(Directions.RIGHT);
+        }
+        else if(Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed){
+          pressedDirections.Add(Directions.LEFT);
+        }
+        if(Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed){
+          pressedDirections.Add(Directions.UP);
+        }
+        else if(Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed){
+          pressedDirections.Add(Directions.DOWN);
+        }
       }
-      else if(Keyboard.current.aKey.isPressed){
-        pressedDirections.Add(Directions.LEFT);
+
+      if(pressedDirections.Count > 0){
+        updateVelocity(pressedDirections);
+        targetPosition.x = -1;
+        targetPosition.y = -1;
       }
-      if(Keyboard.current.wKey.isPressed){
-        pressedDirections.Add(Directions.UP);
-      }
-      else if(Keyboard.current.sKey.isPressed){
-        pressedDirections.Add(Directions.DOWN);
-      }
-        
-      updateVelocity(pressedDirections);
+      else if(targetPosition.x > 0 && targetPosition.y > 0)
+        updateVelocity(targetPosition);
+      else
+        updateVelocity(pressedDirections);
       updatePosition();
     }
 
